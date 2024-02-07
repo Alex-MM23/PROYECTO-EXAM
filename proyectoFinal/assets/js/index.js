3c75060eebc229df7f30146ef43b92885534b094
Manu <44340862+ManuelDC-web@users.noreply.github.com>
2024-02-07 13:38:55 +0100

    fronted actualizado y archivo JSON

--- proyectoFinal/assets/js/index.js
+++ proyectoFinal/assets/js/index.js
@@ -1,17 +1,7 @@
-var mysql = require('mysql');
+function login(){
+    window.location.href = 'login.html';
+}
 
-var conexion = mysql.createConnection({
-    host: 'localhost',
-    database: 'vacantes_BBDD_2024_EXA',
-    user: 'uvariantes',
-    password: 'uvariantes'
-});
-
-conexion.connect(function(error){
-    if (error){
-        throw error;
-    }else{
-        console.log('CONEXION EXITOSA');
-    }
-});
-conexion.end();
+function registro(){
+    window.location.href = 'registro.html';
+}
