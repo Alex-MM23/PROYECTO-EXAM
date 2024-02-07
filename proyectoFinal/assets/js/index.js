352367511cc468168c509481db89f3b69e5c1ad5
AlexAttomo <104031928+Alex-MM23@users.noreply.github.com>
2024-02-07 13:48:29 +0100

    1

--- proyectoFinal/assets/js/index.js
+++ proyectoFinal/assets/js/index.js
@@ -1,7 +1,23 @@
-function login(){
-    window.location.href = 'login.html';
-}
+fetch(bbddJSON.json)
+    .then((res) => res.json())
+    .then((Categorias) => {
+        Categorias.forEach((categoria) => {
+            let divProducto = document.createElement("div");
+            divProducto.classList.add("divProductos");
+            let pID = document.createElement("p");
+            let pDescripcion = document.createElement("p");
 
-function registro(){
-    window.location.href = 'registro.html';
-}
+            divProducto.addEventListener("click", () => {
+                cargarProductosPorFamilia(categoria.idFamilia);
+            });
+
+            pID.innerHTML = `${categoria.nombre}`;
+            pDescripcion.innerHTML = categoria.descripcion;
+
+
+            divProducto.appendChild(pDescripcion);
+            divProducto.style.border = "solid black 2px";
+
+            divFamilia.appendChild(divProducto);
+        });
+    });
