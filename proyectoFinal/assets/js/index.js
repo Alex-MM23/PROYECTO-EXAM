e7255622c758897234f29d1b56f90577b384915f
AlexAttomo <104031928+Alex-MM23@users.noreply.github.com>
2024-02-09 14:26:56 +0100

    1

--- proyectoFinal/assets/js/index.js
+++ proyectoFinal/assets/js/index.js
@@ -1,6 +1,13 @@
 let divProductos = document.getElementById("categorias");
+const urlParams = new URLSearchParams(window.location.search);
+const idCategoria = urlParams.get("idCategoria");
 
 let urlFetchAll = "http://localhost:8084/apirest/categoria/todos";
+
+function cargarVacantesPorCategoria(idCategoria) {
+    // Realizar redirección a la nueva página pasando el ID de la familia
+    window.location.href = `categorias.html?idCategoria=${idCategoria}`;
+}
 
 fetch(urlFetchAll)
 .then(res => res.json())
@@ -13,6 +20,10 @@ fetch(urlFetchAll)
         let pDescripcion = document.createElement("p");
         pDescripcion.classList.add("categoria-descripcion")
 
+        divCategoria.addEventListener("click", () => {
+            cargarVacantesPorCategoria(categoria.idCategoria);
+        });
+
         pID.innerHTML = categoria.nombre;
         pDescripcion.innerHTML = categoria.descripcion;
 
@@ -22,6 +33,41 @@ fetch(urlFetchAll)
         divProductos.appendChild(divCategoria);
         });
 })
+
+if (idCategoria) {
+    let urlProductosPorFamilia = `http://localhost:8085/apirest/categorias/porCategoria/${idCategoria}`;
+    let divProductos = document.getElementById("productosPorFamilia");
+
+    fetch(urlProductosPorFamilia)
+        .then((res) => res.json())
+        .then((productos) => {
+            productos.forEach((producto) => {
+                let divProducto = document.createElement("div");
+                let pDescripcion = document.createElement("p");
+                let pMarca = document.createElement("p");
+                let pPrecioUnitario = document.createElement("p");
+
+                pDescripcion.innerHTML = `${producto.descripcion}`;
+                pMarca.innerHTML = `Marca: ${producto.marca}`;
+                pPrecioUnitario.innerHTML = `Precio: ${producto.precioUnitario}€`;
+
+                divProducto.appendChild(pDescripcion);
+                divProducto.appendChild(pMarca);
+                divProducto.appendChild(pPrecioUnitario);
+                divProducto.style.border = "solid black 2px";
+
+                divProductos.appendChild(divProducto);
+            });
+        })
+        .catch((error) => {
+            console.error(
+                "Hubo un error al obtener los productos por familia:",
+                error
+            );
+        });
+} else {
+    console.error("No se proporcionó un ID de familia en la URL.");
+}
 
 function login(){
     window.location.href = 'login.html';
