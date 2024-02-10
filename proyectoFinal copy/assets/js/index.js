c5a843dc19cc4c12ca363ecd9320021f0cdf3cda
Manu <44340862+ManuelDC-web@users.noreply.github.com>
2024-02-10 18:50:16 +0100

    actualizacion

---
+++ proyectoFinal copy/assets/js/index.js
@@ -0,0 +1,33 @@
+let divFamilia = document.getElementById("productos");
+
+fetch(bbddJSON.json)
+    .then((res) => res.json())
+    .then((Categorias) => {
+        Categorias.forEach((categoria) => {
+            let divProducto = document.createElement("div");
+            divProducto.classList.add("divProductos");
+            let pID = document.createElement("p");
+            let pDescripcion = document.createElement("p");
+
+            divProducto.addEventListener("click", () => {
+                cargarProductosPorFamilia(categoria.idFamilia);
+            });
+
+            pID.innerHTML = `${categoria.nombre}`;
+            pDescripcion.innerHTML = categoria.descripcion;
+
+
+            divProducto.appendChild(pDescripcion);
+            divProducto.style.border = "solid black 2px";
+
+            divFamilia.appendChild(divProducto);
+        });
+    });
+
+function login(){
+    window.location.href = 'login.html';
+}
+
+function registro(){
+    window.location.href = 'registro.html';
+}
