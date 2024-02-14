6fc6890512b174a676688a8cce8ff1e07ce40ec8
AlexAttomo <104031928+Alex-MM23@users.noreply.github.com>
2024-02-14 13:07:36 +0100

    Usuarios

---
+++ proyectoFinal copy/assets/js/jsUsuario.js
@@ -0,0 +1,205 @@
+let divFCategoria = document.getElementById("categoriasU");
+
+let urlFetchAll = "http://localhost:8084/apirest/categoria/todos";
+
+function cargarVacantesPorCategoria(idCategoria) {
+  // Realizar redirección a la nueva página pasando el ID de la familia
+  window.location.href = `categoriasU.html?idCategoria=${idCategoria}`;
+}
+
+fetch(urlFetchAll)
+  .then((res) => res.json())
+  .then((categorias) => {
+    categorias.forEach((categoria) => {
+      let pImagen = document.createElement("img");
+      pImagen.src = "assets/IMG/" + categoria.imagen;
+      pImagen.classList.add("section-imagen");
+      let divCategoria = document.createElement("div");
+      divCategoria.classList.add("section-categoria");
+      let pID = document.createElement("p");
+      pID.classList.add("categoria-nombre");
+      let pDescripcion = document.createElement("p");
+      pDescripcion.classList.add("categoria-descripcion");
+
+      divCategoria.addEventListener("click", () => {
+        cargarVacantesPorCategoria(categoria.idCategoria);
+      });
+
+      pID.innerHTML = categoria.nombre;
+      pDescripcion.innerHTML = categoria.descripcion;
+
+      divCategoria.appendChild(pID);
+      divCategoria.appendChild(pDescripcion);
+
+      divFCategoria.appendChild(divCategoria);
+    });
+  });
+
+const urlParams = new URLSearchParams(window.location.search);
+const idCategoria = urlParams.get("idCategoria");
+
+if (idCategoria) {
+  let urlProductosPorFamilia = `http://localhost:8084/apirest/categoria/porCategoria/${idCategoria}`;
+  let divProductos = document.getElementById("vacantesU");
+
+  fetch(urlProductosPorFamilia)
+    .then((res) => res.json())
+    .then((vacantes) => {
+      vacantes.forEach((vacante) => {
+        let pImagen = document.createElement("img");
+        pImagen.src = "assets/IMG/" + vacante.imagen;
+        pImagen.classList.add("section-imagen");
+        let divProducto = document.createElement("div");
+        divProducto.classList.add("section-vacante");
+        let pDescripcion = document.createElement("p");
+        pDescripcion.classList.add("section-descripcion");
+        let pNombre = document.createElement("p");
+        pNombre.classList.add("section-nombre");
+        let pFecha = document.createElement("p");
+        pFecha.classList.add("section-fecha");
+        let pSalario = document.createElement("p");
+        pSalario.classList.add("section-salario");
+        let pEstatus = document.createElement("p");
+        pEstatus.classList.add("section-estatus");
+        let pDetalles = document.createElement("p");
+        pDetalles.classList.add("section-detalles");
+
+        divProducto.addEventListener("click", () => {
+          cargarDetalleVacante(vacante.idVacante);
+        });
+        pImagen.innerHTML = `${vacante.imagen}`;
+        pNombre.innerHTML = `${vacante.nombre}`;
+        pDescripcion.innerHTML = `${vacante.descripcion}`;
+        pFecha.innerHTML = `Fecha de publicación: ${vacante.fecha}`;
+        pSalario.innerHTML = `Salario Anual: ${vacante.salario}€`;
+        pEstatus.innerHTML = `${vacante.estatus}`;
+
+        pDetalles.innerHTML = `${vacante.detalles}`;
+
+        divProducto.appendChild(pImagen);
+        divProducto.appendChild(pNombre);
+        divProducto.appendChild(pDescripcion);
+        divProducto.appendChild(pFecha);
+        divProducto.appendChild(pSalario);
+        divProducto.appendChild(pEstatus);
+        divProducto.appendChild(pDetalles);
+
+        divProductos.appendChild(divProducto);
+      });
+    })
+    .catch((error) => {
+      console.error(
+        "Hubo un error al obtener los productos por familia:",
+        error
+      );
+    });
+} else {
+  console.error("No se proporcionó un ID de familia en la URL.");
+}
+
+function cargarDetalleVacante(idVacante) {
+  // Realizar redirección a la nueva página pasando el ID de la familia
+  window.location.href = `DetalleU.html?idVacante=${idVacante}`;
+}
+
+const idVacante = urlParams.get("idVacante");
+
+if (idVacante) {
+  let urlVacante = `http://localhost:8084/apirest/categoria/vacanteDetalle/${idVacante}`;
+  let divProductos = document.getElementById("detalleU");
+
+  fetch(urlVacante)
+    .then((res) => res.json())
+    .then((vacantes) => {
+      vacantes.forEach((vacante) => {
+        let pImagen = document.createElement("img");
+        pImagen.src = "assets/IMG/" + vacante.imagen;
+        pImagen.classList.add("section-imagen");
+        let divProducto = document.createElement("div");
+        divProducto.classList.add("section-vacante");
+        let pNombre = document.createElement("p");
+        pNombre.classList.add("section-nombre");
+        let pDescripcion = document.createElement("p");
+        pDescripcion.classList.add("section-descripcion");
+        let pFecha = document.createElement("p");
+        pFecha.classList.add("section-fecha");
+        let pSalario = document.createElement("p");
+        pSalario.classList.add("section-salario");
+        let pEstatus = document.createElement("p");
+        pEstatus.classList.add("section-estatus");
+
+        pImagen.innerHTML = `${vacante.imagen}`;
+        pNombre.innerHTML = `${vacante.nombre}`;
+        pDescripcion.innerHTML = `${vacante.descripcion}`;
+        pFecha.innerHTML = `Fecha de publicación: ${vacante.fecha}`;
+        pSalario.innerHTML = `Salario Anual: ${vacante.salario}€`;
+        pEstatus.innerHTML = `${vacante.estatus}`;
+
+        divProducto.appendChild(pImagen);
+        divProducto.appendChild(pNombre);
+        divProducto.appendChild(pDescripcion);
+        divProducto.appendChild(pFecha);
+        divProducto.appendChild(pSalario);
+        divProducto.appendChild(pEstatus);
+
+        divProductos.appendChild(divProducto);
+      });
+    })
+    .catch((error) => {
+      console.error(
+        "Hubo un error al obtener los productos por familia:",
+        error
+      );
+    });
+} else {
+  console.error("No se proporcionó un ID de familia en la URL.");
+}
+
+let urlVacante = `http://localhost:8084/apirest/categoria/todosVacantes`;
+let divVacantesTodos = document.getElementById("vacantesTodosU");
+
+fetch(urlVacante)
+  .then((res) => res.json())
+  .then((vacantes) => {
+    vacantes.forEach((vacante) => {
+      let pImagen = document.createElement("img");
+      pImagen.src = "assets/IMG/" + vacante.imagen;
+      pImagen.classList.add("section-imagen");
+      let divProducto = document.createElement("div");
+      divProducto.classList.add("section-vacante");
+      let pNombre = document.createElement("p");
+      pNombre.classList.add("section-nombre");
+      let pDescripcion = document.createElement("p");
+      pDescripcion.classList.add("section-descripcion");
+      let pFecha = document.createElement("p");
+      pFecha.classList.add("section-fecha");
+      let pSalario = document.createElement("p");
+      pSalario.classList.add("section-salario");
+      let pEstatus = document.createElement("p");
+      pEstatus.classList.add("section-estatus");
+      let pDetalles = document.createElement("p");
+      pDetalles.classList.add("section-detalles");
+
+      divProducto.addEventListener("click", () => {
+        cargarDetalleVacante(vacante.idVacante);
+      });
+      pImagen.innerHTML = `${vacante.imagen}`;
+      pNombre.innerHTML = `${vacante.nombre}`;
+      pDescripcion.innerHTML = `${vacante.descripcion}`;
+      pFecha.innerHTML = `Fecha de publicación: ${vacante.fecha}`;
+      pSalario.innerHTML = `Salario Anual: ${vacante.salario}€`;
+      pEstatus.innerHTML = `${vacante.estatus}`;
+
+      pDetalles.innerHTML = `${vacante.detalles}`;
+
+      divProducto.appendChild(pImagen);
+      divProducto.appendChild(pNombre);
+      divProducto.appendChild(pDescripcion);
+      divProducto.appendChild(pFecha);
+      divProducto.appendChild(pSalario);
+      divProducto.appendChild(pEstatus);
+      divProducto.appendChild(pDetalles);
+
+      divVacantesTodos.appendChild(divProducto);
+    });
+  });
