8a3c5f7b18525d4de23145e6e405472bfd14dd5b
Manu <44340862+ManuelDC-web@users.noreply.github.com>
2024-02-18 22:20:09 +0100

    aqui

---
+++ Tomas_Backend_Rest_Vacantes_OK/src/main/java/vacantes/modelo/entidades/dao/VacanteDaoImplMy8.java
@@ -0,0 +1,51 @@
+package vacantes.modelo.entidades.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import vacantes.modelo.entidades.Vacante;
+import vacantes.modelo.repository.VacanteRepository;
+
+@Repository
+public class VacanteDaoImplMy8 implements VacanteDao{
+
+	@Autowired
+	private VacanteRepository vrepo;
+	
+	@Override
+	public List<Vacante> findAll() {
+		// TODO Auto-generated method stub
+		return vrepo.findAll();
+	}
+
+	@Override
+	public Vacante findById(int idVacante) {
+		// TODO Auto-generated method stub
+		return vrepo.findById(idVacante).orElse(null);
+	}
+
+	@Override
+	public Vacante insertOne(Vacante vacante) {
+		// TODO Auto-generated method stub
+		try {
+			return vrepo.save(vacante);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Vacante> findByCategoria(int idCategoria) {
+		// TODO Auto-generated method stub
+		return vrepo.productosPorCategoria(idCategoria);
+	}
+
+	@Override
+	public List<Vacante> vacantePorDetalle(int idVacante) {
+		// TODO Auto-generated method stub
+		return vrepo.vacanteDetalle(idVacante);
+	}
+
+}
